4f7400debbf33bf16199ecedd88143547710a6bf
Fred James <fredjsam@live.co.uk>
2020-03-12 23:03:29 +0000

    Demo App

---
+++ BlazorDemo.Server/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorDemo.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorDemo.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        [Route("all")]
+        public IEnumerable<PersonModel> GetPeople()
+        {
+            return new List<PersonModel>
+            {
+                new PersonModel { FirstName = "Tim", LastName = "Corey", AccountBalance = 19.45M },
+                new PersonModel { FirstName = "Mary", LastName = "Jones", AccountBalance = 105.87M },
+                new PersonModel { FirstName = "John", LastName = "Smith", AccountBalance = 115.32M }
+            };
+        }
+
+        [HttpPost]
+        public void Post(PersonModel person)
+        {
+
+        }
+    }
+}
